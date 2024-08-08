b21dc210ac3299a77fa8e48fae986d295c656288
adiatraa <adiatrahman31@gmail.com>
2024-08-08 21:33:35 +0700

    Update Fix Hero Section

--- newsglo/src/app/page.tsx
+++ newsglo/src/app/page.tsx
@@ -5,12 +5,12 @@ import display from "../../public/display.jpg";
 export default function Home() {
   return (
     <main className="flex bg-white">
-      <div className="p-24 flex">
-        <div className="p-4 w-2/5 flex flex-col gap-6">
+      <div className="p-24 flex gap-10 w-screen 2xl:h-[800px]">
+        <div className="p-4 w-2/6 flex flex-col lg:gap-6 2xl:gap-8 2xl:justify-center">
           <h1 className="text-sm text-blue-500">INNOVATION</h1>
-          <h1 className="text-xl font-bold">Charge Two Devices at the Same Time With This Magnetic Wireless Charging Dock
+          <h1 className="lg:text-xl 2xl:text-4xl font-bold 2xl:leading-[50px]">Charge Two Devices at the Same Time With This Magnetic Wireless Charging Dock
           </h1>
-          <p className="text-xs leading-6 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p className="lg:text-xs 2xl:text-md lg:leading-6 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           <div className="flex gap-4 items-center">
             <div className="h-10 w-10 bg-gray-500 rounded-full">
               <p>.</p>
@@ -21,11 +21,11 @@ export default function Home() {
             </div>
           </div>
           <Link href="#">
-              <button className="btn btn-sm btn-outline">Read More</button>
+            <button className="btn btn-sm btn-outline">Read More</button>
           </Link>
         </div>
-        <div className="w-3/5 relative">
-          <Image src={display} alt="image1" className="object-contain" height={100} width={700} />
+        <div className="w-2/3 relative">
+          <Image src={display} alt="image1" className="object-cover rounded-md" fill />
         </div>
       </div>
     </main>
